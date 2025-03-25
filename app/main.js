5b81936ada019eef8946fd78eef81f2da0c0bc49
Adriano Saadeh <adrianosaadeh@gmail.com>
2025-03-25 07:16:01 -0300

    busca dados dinamicamente da url de livros

---
+++ app/main.js
@@ -0,0 +1,10 @@
+let livros = [];
+const endpointDaApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+
+getBuscarLivrosDaApi();
+
+async function getBuscarLivrosDaApi() {
+    const response = await fetch(endpointDaApi);
+    livros = await response.json();
+    console.table(livros);
+}
